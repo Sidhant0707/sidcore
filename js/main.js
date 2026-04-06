626df3b9a0879133d98a386f41c40ed46f4531c0
Sidhant Kumar <csds23108@glbitm.ac.in>
2026-04-06 20:04:40 +0530

    Add basic Three.js scene with particles animation

--- js/main.js
+++ js/main.js
@@ -32,3 +32,82 @@ behavior:"smooth"
 })
 
 })
+
+
+
+const scene = new THREE.Scene()
+
+const camera = new THREE.PerspectiveCamera(
+75,
+window.innerWidth/window.innerHeight,
+0.1,
+1000
+)
+
+const renderer = new THREE.WebGLRenderer({
+canvas:document.querySelector("#bg"),
+alpha:true
+})
+
+renderer.setSize(window.innerWidth,window.innerHeight)
+
+camera.position.z=5
+
+
+
+const particlesGeometry=new THREE.BufferGeometry()
+
+const particlesCount=800
+
+const posArray=new Float32Array(particlesCount*3)
+
+for(let i=0;i<particlesCount*3;i++){
+
+posArray[i]=(Math.random()-0.5)*10
+
+}
+
+particlesGeometry.setAttribute(
+"position",
+new THREE.BufferAttribute(posArray,3)
+)
+
+const particlesMaterial=new THREE.PointsMaterial({
+
+size:0.01,
+color:0xffffff
+
+})
+
+const particlesMesh=new THREE.Points(
+particlesGeometry,
+particlesMaterial
+)
+
+scene.add(particlesMesh)
+
+
+
+function animate(){
+
+requestAnimationFrame(animate)
+
+particlesMesh.rotation.y+=0.0005
+
+renderer.render(scene,camera)
+
+}
+
+animate()
+
+
+
+window.addEventListener("resize",()=>{
+
+camera.aspect=window.innerWidth/window.innerHeight
+
+camera.updateProjectionMatrix()
+
+renderer.setSize(window.innerWidth,window.innerHeight)
+
+})

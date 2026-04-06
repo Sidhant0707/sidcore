402282fa29c8f88861fda32b2533ad54fa06b821
Sidhant Kumar <csds23108@glbitm.ac.in>
2026-04-06 19:58:04 +0530

    Add smooth scrolling and reveal animations

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
+const lenis = new Lenis()
 
+function raf(time){
+lenis.raf(time)
+requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+
+
+gsap.from(".reveal",{
+
+opacity:0,
+y:40,
+duration:1,
+stagger:0.2,
+ease:"power3.out"
+
+})
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+
+anchor.addEventListener("click", function(e){
+
+e.preventDefault()
+
+document.querySelector(this.getAttribute("href")).scrollIntoView({
+behavior:"smooth"
+})
+
+})
+
+})

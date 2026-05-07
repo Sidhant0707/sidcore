773b51507e37704614d89e7cb9eef67c2b1af1f5
Sidhant0707 <sidhantkumar0707@gmail.com>
2026-05-07 16:26:25 +0530

    chore: setup github actions for static nextjs export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Forces Next.js to generate a static 'out' folder
+  images: {
+    unoptimized: true, // Required for static export
+  },
 };
 
 export default nextConfig;
